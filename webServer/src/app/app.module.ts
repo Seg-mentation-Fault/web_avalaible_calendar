b8bbdb58ba2a2b0fac00c8779bdda044f4198ba0
jdcera4 <jdcera4@gmail.com>
2021-10-12 17:51:17 -0500

    import compoent

--- webServer/src/app/app.module.ts
+++ webServer/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './numberImputCapture/app.component';
-import { NavbarcomponentComponent } from './navbarcomponent/navbarcomponent.component';
+import { NavbarcomponentComponent } from './navbar/navbart.component';
 
 @NgModule({
   declarations: [
+    NavbarcomponentComponent,
     AppComponent,
-    NavbarcomponentComponent
   ],
   imports: [
     BrowserModule,
