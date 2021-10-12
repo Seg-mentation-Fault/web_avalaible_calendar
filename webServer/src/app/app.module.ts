2d5f86af1f342dca97582647b50f303a0a174a1c
jdcera4 <jdcera4@gmail.com>
2021-10-12 17:05:03 -0500

    crete navar component

--- webServer/src/app/app.module.ts
+++ webServer/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './numberImputCapture/app.component';
+import { NavbarcomponentComponent } from './navbarcomponent/navbarcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarcomponentComponent
   ],
   imports: [
     BrowserModule,
